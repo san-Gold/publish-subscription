098f176ba95bf57ccd47e20d23ed3b176f92ed05
Rick <50647350+san-Gold@users.noreply.github.com>
2022-07-17 10:41:50 +0800

    初始化

---
+++ 发布订阅/b.js
@@ -0,0 +1,3 @@
+btn2.onclick = () => {
+  $emit("btn2", "b");
+};
---
+++ 发布订阅/c.js
@@ -0,0 +1,15 @@
+$on("btn2", (val) => {
+  console.log(1, val);
+});
+$on("btn2", (val) => {
+  console.log(2, val);
+});
+$on("btn2", (val) => {
+  console.log(3, val);
+});
+$on("btn2", (val) => {
+  console.log(4, val);
+});
+$on("btn2", (val) => {
+  console.log(5, val);
+});
